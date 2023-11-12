50330eefaaa09f2aeb3170f5ce7b3b75c06e7138
quadri090 <99067374+quadri090@users.noreply.github.com>
2023-11-12 19:14:25 +0100

    fix: build error

--- src/Pages/OrderSuccessfull.jsx
+++ src/Pages/OrderSuccessfull.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { HomeContext } from "../context/HomeContext.jsx";
 import successful from '../assets/successful.png'
 import Btn from '../components/Btn'
 
 export default function OrderSuccessfull() {
+    const {
+        clearCart
+      } = useContext(HomeContext);
+
   return (
     <div className='flex flex-col items-center justify-center mt-[150px]  font-Poppins text-center'>
         <div className='w-[200px] h-[250px] py-auto'>
@@ -13,7 +18,7 @@ export default function OrderSuccessfull() {
                 <p className='text-[20px] font-medium text-[#5EA304] '>Order Successful</p>
                 <p className='text-[15px] text-primary-grey'>We have started <br /> processing your order.</p>
             </div>
-            <div className='w-[200px] mt-6'>
+            <div className='w-[200px] mt-6' onClick={() => clearCart()}>
                 <Btn 
                     routte='/'
                     text='Home'
